c4c84ce1a36f8c25e679141bfd923eb2ff65cc9b
vuduynewaccount <95763704+vuduynewaccount@users.noreply.github.com>
2022-09-07 00:28:00 +0700

    Update lesson.go

--- model/lesson.go
+++ model/lesson.go
@@ -34,8 +34,7 @@ func(s *Lesson) Create(c *fiber.Ctx){
   dbLesson.Min_read =s.Min_read
   dbLesson.Data     =s.Data
   dbLesson.View = 0;// mac dinh luc tao chua co view nao
-  fmt.Println(dbLesson)
-  // database.DB.Create(&dbLesson);
+  database.DB.Create(&dbLesson);
 }
 
 
