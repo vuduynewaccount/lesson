62836e8cd487649ca795bef2b9d1d970ae42ab5e
vuduynewaccount <95763704+vuduynewaccount@users.noreply.github.com>
2022-09-07 01:14:32 +0700

    Update client-list.go

--- routes/implement/client-list.go
+++ routes/implement/client-list.go
@@ -7,7 +7,7 @@ import(
 
 func List(c *fiber.Ctx) error {
   var listLesson *[]database.DbLesson;
-  database.DB.Raw("SELECT * FROM db_lessons").Scan(listLesson);
+  database.DB.Raw("SELECT * FROM db_lessons").Scan(&listLesson);
   resp:=new(model.Resp);
   resp.Constructor(1,"get list thanh cong",listLesson)
   return c.JSON(resp)
