1de7f6ae44f30496dd75f6619a2343ab0b97b991
vuduynewaccount <95763704+vuduynewaccount@users.noreply.github.com>
2022-09-07 01:19:40 +0700

    Update admin-delete.go

--- routes/implement/admin-delete.go
+++ routes/implement/admin-delete.go
@@ -1,10 +1,16 @@
 package implement
 import(
   "github.com/gofiber/fiber/v2"
-  "fmt"
 )
 
 func AdminDelete(c *fiber.Ctx) error {
-  fmt.Println(c.AllParams()["id"])
-  return c.SendString("admin delete post")
+  lesson:=new(model.Lesson);
+
+
+  lesson.Delete(c.AllParams()["id"])// truyen id vao
+
+  resp:=new(model.Resp);
+  resp.Constructor(1,"delete successly","")
+
+  return c.JSON(resp)
 }
